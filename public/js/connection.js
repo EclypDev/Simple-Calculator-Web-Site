88083e120c78f7078d4b57751625ade528c507ec
ChocolaHD <57846452+ChocolaHD@users.noreply.github.com>
2023-04-28 04:48:53 +0800

    Update connection.js documentation...

--- public/js/connection.js
+++ public/js/connection.js
@@ -1,13 +1,13 @@
-const ws = new WebSocket('ws://localhost:3000');
+const ws = new WebSocket('ws://localhost:3000'); //CONNECT WITH SERVER
 
-const display = document.getElementById('display')
+const display = document.getElementById('display') //OBTANING DISPLAY ELEMENT BY ID
 
-const addNumber = function(value) {
+const addNumber = function(value) { //FUNCTION ADDNUMBER TO DISPLAY
     display.value += value;
 }
-const removeNumber = function(value) {
+const removeNumber = function(value) { //FUNCTION REMOVENUMBER TO DISPLAY
     display.value = ""
 }
-const result = function() {
+const result = function() { //FUNCTION TO GIVE RESULT TO DISPLAY
     display.value = eval(display.value);
 }
